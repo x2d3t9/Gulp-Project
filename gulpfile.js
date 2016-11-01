29a84415390b9232e240d446064256d8068fbee1
X2D3T9 <settgetter@gmail.com>
2016-11-01 12:30:29 +0200

    notify add in the project

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ const gulp 	 	 = require('gulp'),
 	  concat 	 = require('gulp-concat'),
 	  coffee 	 = require('gulp-coffee'),
 	  es 	 	 = require('event-stream'),
-	  sass	 	 = require('gulp-ruby-sass'),
+	  sass	 	 = require('gulp-sass'),
 	  sourcemaps = require('gulp-sourcemaps'),
 	  plumber	 = require('gulp-plumber'),
 	  jade		 = require('gulp-jade'),
-	  connect    = require('gulp-connect');
+	  connect    = require('gulp-connect'),
+	  autoprefixer = require('gulp-autoprefixer'),
+	  notify     = require('gulp-notify');
 	  
 
 
@@ -18,7 +20,7 @@ gulp.task('scripts', () => {
 		.pipe(coffee());
 	let js = gulp.src('src/js/**/*.js');
 	return es.merge(jsScriptFromCoffe, js)
-	.pipe(plumber())
+	.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
 	.pipe(concat('all.min.js'))
 	.pipe(uglify())
 	.pipe(connect.reload())
@@ -27,23 +29,24 @@ gulp.task('scripts', () => {
 
 
 //Style Tasck
-gulp.task('sass', () => {
-    sass('src/scss/**/*.scss', {sourcemap: true})
-        .on('error', sass.logError)
-        .pipe(sourcemaps.write())
-        .pipe(sourcemaps.write('maps', {
-            includeContent: false,
-            sourceRoot: 'source'
-        }))
-        .pipe(connect.reload())
-        .pipe(gulp.dest('app/css'));
+gulp.task('sass',  () => {
+ return gulp.src('./src/scss/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+  }))
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(concat('all.min.css'))
+  .pipe(sourcemaps.write('./maps'))
+  .pipe(connect.reload())
+  .pipe(gulp.dest('./app/css'));
 });
 
 
 //HTML Template
 gulp.task('jade', () => {
 	return gulp.src('src/template/**/*.jade')
-		.pipe(plumber())
 		.pipe(jade())
 		.pipe(connect.reload())
 		.pipe(gulp.dest('app'));
